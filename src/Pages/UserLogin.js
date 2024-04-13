b4fea40c2e64ec72e31c36de5279ec058614ff7f
AbdulHaseebAhmad <79489830+AbdulHaseebAhmad@users.noreply.github.com>
2024-04-14 02:46:06 +0300

    Error Precaussion

--- src/Pages/UserLogin.js
+++ src/Pages/UserLogin.js
@@ -11,20 +11,28 @@ export default function UserLogin() {
 
 
 export const loginAction = async ({ request, params }) => {
-  const formData = await request.json();
-  console.log(formData);
+  try {
+    const formData = await request.json();
+    console.log(formData);
 
-  const loginRequest = fetch('http://127.0.0.1:8001/login',{
-    method:request.method,
-    mode:'cors',
-    headers:{
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(formData)
-  })
-  const loginRequestData = await loginRequest.json();
-  if(!loginRequestData.status){
-    redirect('/adminLogin')
+    const loginRequest = await fetch('http://127.0.0.1:8001/login', {
+      method: request.method,
+      // mode: 'no-cors',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData)
+    });
+
+    if (!loginRequest.ok) {
+      throw new Error('Failed to fetch');
+       
+    }
+
+    const loginRequestData = await loginRequest.json();
+    return loginRequestData;
+  } catch (error) {
+    console.error('Error:', error);
+    return redirect("/adminLogin")
   }
-  return loginRequestData;
 }
