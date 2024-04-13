6fd8a49264bea1ae1514a0f2f42210df08e22616
AbdulHaseebAhmad <79489830+AbdulHaseebAhmad@users.noreply.github.com>
2024-04-14 00:04:41 +0300

    Welcome Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './App.css';
+import WwelcomePage from "./Pages/WelcomePage";
 
 function App() {
   const router = createBrowserRouter([
     {
       path:'/',
       children:[
-        {index:true,element:'welcomePage',},
+        {index:true,element:<WwelcomePage/>,},
         {path:'userLogin',element:'loginPage'},
         {path:'adminLogin',element:'loginPage'},
         {path:'/home',element:'homePage',}
--- src/Pages/WelcomePage.js
+++ src/Pages/WelcomePage.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export default function welcomePage() {
+export default function WwelcomePage() {
   return (
     <div>
-      
+      <div className='flex justify-center space-x-20 items-center my-40' >
+      <button className='rounded px-12 font-bold text-2xl py-5 hover:bg-green-400 bg-green-600 text-white'  >Login</button>
+      <button className='rounded px-12 font-bold text-2xl py-5 hover:bg-green-400 bg-green-600 text-white'  >Admin</button>
+      </div>
     </div>
   )
 }
