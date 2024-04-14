137fe2a52b1bd29a4ccdd0ba9641ed51c8707899
AbdulHaseebAhmad <79489830+AbdulHaseebAhmad@users.noreply.github.com>
2024-04-14 04:46:40 +0300

    Route Blocking

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './App.css';
 import WelcomePage from "./Pages/WelcomePage";
-import UserLogin, {loginAction} from "./Pages/UserLogin";
+import UserLogin, {loginAction, loginLoader} from "./Pages/UserLogin";
 import AdminLogin from "./Pages/AdminLogin";
 import Home, {homePageLoaderFunction}  from "./Pages/Home";
 function App() {
@@ -10,7 +10,7 @@ function App() {
       path:'/',
       children:[
         {index:true,element:<WelcomePage/>,},
-        {path:'/userLogin',element:<UserLogin/>, action:loginAction},
+        {path:'/userLogin',element:<UserLogin/>, action:loginAction,loader:loginLoader},
         {path:'/adminLogin',element:<AdminLogin/>},
         {path:'/home',element:<Home/>,loader:homePageLoaderFunction}
       ]
