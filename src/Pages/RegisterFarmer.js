2c5dd0648a525c7187e7bdb0c54dd92792d06de2
AbdulHaseebAhmad <79489830+AbdulHaseebAhmad@users.noreply.github.com>
2024-04-15 21:55:07 +0300

    TT

--- src/Pages/RegisterFarmer.js
+++ src/Pages/RegisterFarmer.js
@@ -39,8 +39,7 @@ export default function RegisterFarmer() {
   return (
     <div className={classes.contentContainer}>
       <div className={classes.contentHolder}>
-        <form onSubmit={submitData} className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12"
-        >
+        <form onSubmit={submitData} className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
           <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
             <div className="relative py-3 sm:max-w-xl sm:mx-auto">
               <div className="absolute inset-0 bg-gradient-to-r from-green-300 to-green-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
