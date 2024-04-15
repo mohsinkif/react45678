e11c422da0c0763090e8eabf6e8661590dbb479d
AbdulHaseebAhmad <79489830+AbdulHaseebAhmad@users.noreply.github.com>
2024-04-15 14:15:14 +0300

    Log Out

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -49,7 +49,6 @@ const Navbar = () => {
   const logOutHandler = () => {
     setLoogedIn(false);
     document.cookie = "token" + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-    window.location.reload()
     navigate('/')
   }
 
--- src/Pages/WelcomePage.js
+++ src/Pages/WelcomePage.js
@@ -1,9 +1,12 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import { Link, redirect } from 'react-router-dom'
 
 
 export default function WelcomePage() {
 
+  useEffect(()=>{
+    window.location.reload();
+  },[])
   
 
   return (
