9a888d2f742af365bd983a708cb8417ce477af8b
AbdulHaseebAhmad <79489830+AbdulHaseebAhmad@users.noreply.github.com>
2024-04-15 16:03:50 +0300

    Test

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -20,28 +20,11 @@ export default function Home() {
 }
 
 export const homePageLoaderFunction = ({params,request}) => {
-  function getCookie(cookieName) {
-    const name = cookieName + "=";
-    const decodedCookie = decodeURIComponent(document.cookie);
-    const cookieArray = decodedCookie.split(';');
-    for(let i = 0; i < cookieArray.length; i++) {
-        let cookie = cookieArray[i];
-        while (cookie.charAt(0) === ' ') {
-            cookie = cookie.substring(1);
-        }
-        if (cookie.indexOf(name) === 0) {
-            return cookie.substring(name.length, cookie.length);
-        }
-    }
-    return null;
-}
-
-const token = getCookie("token");
-if (token) {
-  console.log(token)
-     redirect('/home');
-} else {
-   return redirect('/');
-}
-return token;
+  const tokenExist = document.cookie.split(';').some((item) => item.trim().startsWith("token" + '='));
+  if(!tokenExist){
+    return redirect("/");
+  }
+  else {
+    return tokenExist;
+  }
 }
