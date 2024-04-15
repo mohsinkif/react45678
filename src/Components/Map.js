58b538741d6eb77250b7681e630947e031f61542
AbdulHaseebAhmad <79489830+AbdulHaseebAhmad@users.noreply.github.com>
2024-04-15 09:07:38 +0300

    Map

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,9 +1,36 @@
 import React, { useEffect, useRef } from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, SVGOverlay, Marker, Popup } from 'react-leaflet'
+
+const position = [51.505, -0.09]
+const bounds = [
+  [51.49, -0.08],
+  [51.5, -0.06],
+]
+
 
 const Map = () => {
-  const mapRef = useRef(null);
+    return <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{width:'200%',height:'85vh', marginTop:'12vh'}}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <SVGOverlay attributes={{ stroke: 'red' }} bounds={bounds}>
+      <circle r="35" cx="30" cy="30" fill="blue" />
+      <text x="50%" y="50%" stroke="black">
+        text
+      </text>
+    </SVGOverlay>
+  </MapContainer>
+};
+
+export default Map;
+
+
+
+/**
+ * const mapRef = useRef(null);
 
   useEffect(() => {
     if (!mapRef.current) {
@@ -18,6 +45,4 @@ const Map = () => {
   }, []);
 
   return <div id="map" style={{ width: "100%", height: "87vh",marginTop:'12vh' }} />;
-};
-
-export default Map;
+ */
