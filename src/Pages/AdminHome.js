a6f1ba25754cbc72aa99c4ba3c06ce03cbec3cb3
AbdulHaseebAhmad <79489830+AbdulHaseebAhmad@users.noreply.github.com>
2024-04-15 18:26:03 +0300

    AL

--- src/Pages/AdminHome.js
+++ src/Pages/AdminHome.js
@@ -1,33 +1,51 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import classes from "../CSS/adminhome.module.css";
 import React, { useEffect, useState } from "react";
-  
 import { TiLeaf } from "react-icons/ti";
+
 export default function AdminHome() {
+  
+    const navigate = useNavigate();
+    
+  const [loggedIn, setLoogedIn] = useState(true);
+
+  useEffect(() => {
+    if (!loggedIn) {
+      navigate("/");
+    }
+  }, [loggedIn]);
+
+  const logOutHandler = () => {
+    setLoogedIn(false);
+    document.cookie =
+      "token" + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  };
 
   return (
     <div className={classes.adminHomeContainer}>
       <div className={classes.adminHomeNavContainer}>
         <div className={classes.adminNavHolder}>
-        <nav className="mx-auto border border-green-700 top-0 left-0 right-0 z-10 bg-transparent  bg-opacity-100">
-        <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
-          <Link
-             className="text-2xl text-secondary-400 md:text-5xl font-semibold">
-            <div className="flex items-center">
-              <p className="text-2xl text-green-700">
-                Agri<span className="text-black">Drone</span>
-              </p>
-              <TiLeaf color="green" size={40} />
+          <nav className="mx-auto border border-green-700 top-0 left-0 right-0 z-10 bg-transparent  bg-opacity-100">
+            <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
+              <Link className="text-2xl text-secondary-400 md:text-5xl font-semibold">
+                <div className="flex items-center">
+                  <p className="text-2xl text-green-700">
+                    Agri<span className="text-black">Drone</span>
+                  </p>
+                  <TiLeaf color="green" size={40} />
+                </div>
+              </Link>
+
+              <Link to="/userLogin">
+                <button
+                  onClick={logOutHandler}
+                  className="rounded px-5 font-bold text-2xl py-2 hover:bg-green-600 hover:text-white bg-transparent text-green-600"
+                >
+                  Log Out
+                </button>
+              </Link>
             </div>
-          </Link>
-          
-          <Link to="/userLogin">
-            <button className="rounded px-5 font-bold text-2xl py-2 hover:bg-green-600 hover:text-white bg-transparent text-green-600">
-              Log Out
-            </button>
-          </Link>
-        </div>
-      </nav>
+          </nav>
         </div>
       </div>
 
