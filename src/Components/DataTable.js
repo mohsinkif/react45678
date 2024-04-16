e478e1876094b914b06981acda20ca7336ee7839
AbdulHaseebAhmad <79489830+AbdulHaseebAhmad@users.noreply.github.com>
2024-04-16 23:46:41 +0300

    !!!

--- src/Components/DataTable.js
+++ src/Components/DataTable.js
@@ -11,7 +11,7 @@ export default function DataTable({ data }) {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({data:event.target.value})
+      body: JSON.stringify(event.target.value)
     });
   }
   return (
