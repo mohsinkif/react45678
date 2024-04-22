5a20c0629662a7580172ba7c8d51748f3d52e8d3
AbdulHaseebAhmad <79489830+AbdulHaseebAhmad@users.noreply.github.com>
2024-04-22 11:06:56 +0300

    VTD

--- src/Pages/ViewTransporterData.js
+++ src/Pages/ViewTransporterData.js
@@ -14,7 +14,7 @@ export default function ViewTransporterData() {
 
 
     const [formData,setFormData]=useState({
-        city: '',})
+        tcity: '',})
     
     const handleChange = (e) =>{
     setFormData({
@@ -23,7 +23,7 @@ export default function ViewTransporterData() {
 
     const searchHarvesterHanle = (event) => {
         event.preventDefault();
-        submit({city:formData.city}, {method:'post',action: `/${pathName}` ,encType:'application/json'})
+        submit({tcity:formData.tcity}, {method:'post',action: `/${pathName}` ,encType:'application/json'})
     }
 
   return (
@@ -34,8 +34,8 @@ export default function ViewTransporterData() {
                     <div className="divide-y divide-gray-200">
                       <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
                         <div className="relative">
-                          <input required autoComplete="off" id="city" name="city" type="text" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-rose-600" placeholder="City Name" onChange={handleChange} />
-                          <label htmlFor="city" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">City Name</label>
+                          <input required autoComplete="off" id="tcity" name="tcity" type="text" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-rose-600" placeholder="City Name" onChange={handleChange} />
+                          <label htmlFor="tcity" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">City Name</label>
                         </div>
                         <div className="relative">
                           <button type='submit' className="bg-green-500 text-white rounded-md px-2 py-1" >Search</button>
