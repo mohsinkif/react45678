bd41bd39b2249a0b082c906994c7b3cf57a11d7c
mohsinkif <mohsinullah68@gmail.com>
2024-05-11 03:37:34 -0700

    add logout on movile screen

--- src/Components/MenuOverLay.js
+++ src/Components/MenuOverLay.js
@@ -2,7 +2,7 @@
 import React from "react";
 import CustomLink from "./CustomLink";
 
-const MenuOverlay = ({ links }) => {
+const MenuOverlay = ({ links, logOutHandler }) => {
   return (
     <ul className="flex flex-col py-4 items-center">
       {links.map((link, index) => (
@@ -10,6 +10,10 @@ const MenuOverlay = ({ links }) => {
           <CustomLink href={link.path} title={link.title} />
         </li>
       ))}
+      <li key={"logout"}>
+              <button className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-green-500 hover:border-white" onClick={logOutHandler}>Logout</button>
+
+      </li>
     </ul>
   );
 };
