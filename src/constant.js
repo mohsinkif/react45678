8972aebcd84c569c6590315e9f8d4a79d9792408
mohsinkif <mohsinullah68@gmail.com>
2024-05-12 11:46:49 -0700

    updates constant file

--- src/constant.js
+++ src/constant.js
@@ -1,1 +1,8 @@
-export const backendurl= process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:8001/' : window.location.host+ '/'
+let _url= process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:8001/' : window.location.host+ '/'
+let url;
+if (window.location.protocol === "https:") {
+        url = `https://${_url}`;
+    } else {
+        url = `http://${_url}`;
+}
+export const backendurl= url;
